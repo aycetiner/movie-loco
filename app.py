f68e422a826e2594a48b0d8c42b19abd93a0b570
Ali Cetiner <ayenercetiner@gmail.com>
2022-10-04 18:22:13 -0500

    app.py rollback

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, render_template, request, flash, redirect, session, g, jsonify, current_app
+from flask import Flask, render_template, request, flash, redirect, session, g, jsonify
 import requests
 from flask_debugtoolbar import DebugToolbarExtension
 from sqlalchemy.exc import IntegrityError
@@ -8,7 +8,6 @@ from sqlalchemy.exc import IntegrityError
 
 from forms import PostForm, UserAddForm, LoginForm, ProfileEditForm, ChangePasswordForm
 from models import db, connect_db, User, Post, Likes, Location, Movie
-from flask_admin import Admin
 
 MovieAPIKey='6461108259d95817ace0a23e57345c98'
 GoogleAPIKey = 'AIzaSyCp1jKqmi89h2mhNsKtSZYK3-IT3xEq8go'
@@ -22,22 +21,16 @@ app = Flask(__name__)
 # Get DB_URI from environ variable (useful for production/testing) or,
 # if not set there, use development local db.
 
-uri = (
-    os.environ.get('DATABASE_URL', 'postgresql:///movie_locations'))  # or other relevant config var
-if uri and uri.startswith("postgres://"):
-    uri = uri.replace("postgres://", "postgresql://", 1)
-# rest of connection code using the connection string `uri`
-app.config['SQLALCHEMY_DATABASE_URI'] = uri
+app.config['SQLALCHEMY_DATABASE_URI'] = (
+    os.environ.get('DATABASE_URL', 'postgresql:///movie_locations'))  
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = False
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = True
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "it's a secret")
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "asiri_gizli_anahtar")
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 # app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
 # toolbar = DebugToolbarExtension(app)
-
-with app.app_context():
-    connect_db(app)
+connect_db(app)
 
 
 
